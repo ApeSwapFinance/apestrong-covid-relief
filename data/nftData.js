2fd6f364ce6eaefc65e9ed86f98b0bb8383b304a
Obie Dobo <obiedobo@gmail.com>
2021-05-19 17:57:08 -0500

    updated media urls

--- data/nftData.js
+++ data/nftData.js
@@ -3,21 +3,21 @@ const data = [
     index: 0,
     name: "Golden Banana",
     description: "The ApeSwap Jungle is always full of treasure! It’s important for us apes to give back from our bountiful BANANA harvest, so we decided to launch this exclusive NFT collection in an effort to support the India Crypto Covid Relief Fund. Golden Banana features the most Special BANANA of them all, allowing you to show off your full dedication and support to the Indian Crypto relief cause!",
-    image: "linkToGif",
+    image: "https://github.com/ApeSwapFinance/apestrong-covid-relief/blob/master/media/0.mp4?raw=true",
     uri: "/0"
   },
   {
     index: 1,
     name: "Silver Banana",
     description: "The ApeSwap Jungle is always full of treasure! It’s important for us apes to give back from our bountiful BANANA harvest, so we decided to launch this exclusive NFT collection in an effort to support the India Crypto Covid Relief Fund. Silver Banana features the second most special BANANA, allowing you to show off your dedication for the Indian Crypto relief cause!",
-    image: "linkToGif",
+    image: "https://github.com/ApeSwapFinance/apestrong-covid-relief/blob/master/media/1.mp4?raw=true",
     uri: "/1"
   },
   {
     index: 2,
     name: "Bronze Banana",
     description: "The ApeSwap Jungle is always full of treasure! It’s important for us apes to give back from our bountiful BANANA harvest, so we decided to launch this exclusive NFT collection in an effort to support the India Crypto Covid Relief Fund. Bronze Banana features the third most special BANANA of them all, allowing you to show off your support for the Indian Crypto relief cause!",
-    image: "linkToGif",
+    image: "https://github.com/ApeSwapFinance/apestrong-covid-relief/blob/master/media/2.mp4?raw=true",
     uri: "/2"
   }
 ]
